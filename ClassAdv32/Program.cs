b86b6f8374d561ad1aa20414fe1f32e19ae02706
Ye-jin Lee <s2108@e-mirim.hs.kr>
2023-06-08 08:51:44 +0900

    속성(Property) 실습

--- ClassAdv32/Program.cs
+++ ClassAdv32/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,20 @@ namespace ClassAdv32
             Product product1 = new Product("과자", 1000);
             Product product2 = new Product("아이스크림", 1000);
             Product product3 = new Product("케이크", 1000);
+
+            // 속성(Property) 실습
+            Box box = new Box(10, 10);
+            box.Width = 100;
+            box.Height = 100;
+            box.GetArea();
+            try
+            {
+                box.Width = -10;    // 잘못된 값이 들어간 경우
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
